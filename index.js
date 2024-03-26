50f8da0addbe517c779253cefc5cb32050d2c62a
Fensley <zagalufe@gmail.com>
2024-03-26 14:48:38 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// Write your JavaScript code here
+
+const input = document.getElementById("input");
+
+input.addEventListener("click" , ()=> {
+    alert("Thanks for joining us . Please check your email to confirm")
+})
